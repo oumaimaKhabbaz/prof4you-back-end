c74bd721d11f74d3bded2f37a2bd44fa04eb0179
isambtb <37896993+isambtb@users.noreply.github.com>
2021-08-10 14:59:09 +0200

    make searc public

--- src/main/java/com/prof4you/app/config/SecurityConfig.java
+++ src/main/java/com/prof4you/app/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeRequests().antMatchers("/account/register/**", "/login/**", "/subjects/**", "/search/**")
+        http.authorizeRequests().antMatchers("/account/register/**", "/login/**", "/subjects/**", "/prof/search/**")
                 .permitAll();
         http.authorizeRequests().antMatchers("/appUsers/**", "/appRoles/**").hasAuthority("ADMIN");
         http.authorizeRequests().anyRequest().authenticated();
